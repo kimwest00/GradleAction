7dcfd61c69f904e75e8c18a29f8096ac50467bd8
minseo <kim-west00@naver.com>
2021-02-02 23:50:32 +0900

    기타 신택스_split,elif,int형변환_수정

--- OneDrive - 한국외국어대학교/바탕 화면/백준예제/BOJ17388.py
+++ OneDrive - 한국외국어대학교/바탕 화면/백준예제/BOJ17388.py
@@ -1,11 +1,15 @@
-participate = input.split()
-#participate는 숭,고,한의 참여도를 담은 리스트
+#participate 변수를 한꺼번에 int로 못바꾸냐,,
+participate = input().split()#AttributeError: 'builtin_function_or_method' object has no attribute 'split'  나니까 괄호쓰라고 야발^^
+participate[0]=int(participate[0])
+participate[1]=int(participate[1])
+participate[2]=int(participate[2])
 if(participate[0]+participate[1]+participate[2]>=100):
     print("OK")
 else:
     if(participate[0]<participate[1] and participate[0]<participate[2]):
         print("Soongsil")
-    else if(participate[1]<participate[0] and participate[1]<participate[2]):
+    elif(participate[1]<participate[0] and participate[1]<participate[2]):
         print("Korea")
-    else if(participate[2]<participate[0] and participate[2]<participate[1]):
+    elif(participate[2]<participate[0] and participate[2]<participate[1]):
         print("Hanyang")
+    #elif -> 파이썬 else if->c언어...민하다 추서야
