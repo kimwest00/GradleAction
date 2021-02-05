463622b9d0461b6fa7b726a338318f1b46d2c236
minseo <kim-west00@naver.com>
2021-02-05 23:39:55 +0900

    BOJ 19637_if문.py

---
+++ OneDrive - 한국외국어대학교/바탕 화면/백준예제/BOJ19637.py
@@ -0,0 +1,15 @@
+#칭호의 개수 N (1 ≤ N ≤ 105)과 칭호를 출력해야 하는 캐릭터들의 개수 M
+#터 N개의 줄에 각 칭호의 이름을 나타내는 길이 1 이상, 11 이하의 영어 대문자로만 구성된 문자열과 해당 칭호의 전투력 상한값을 나타내는 109 이하의 음이 아닌 정수가 주어진다. 
+#칭호는 전투력 상한값의 비내림차순으로 주어진다. 
+
+name_num,charac_num = map(int, input().split())
+for i in range(name_num):
+    name[i]= input()
+    #형이다른 두인자를 띄어쓰기로 받기 좀 애매쓰
+for i in range(charac_num):
+    character[i]=int(input())
+    if(character[i]>):
+        print("")
+    elif( >character[i] >):
+        print("")
+        #name_num만큼 if문 생성
