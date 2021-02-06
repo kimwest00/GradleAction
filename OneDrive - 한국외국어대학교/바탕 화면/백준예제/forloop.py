93c5d8c04e0299f3ba34e60caa39b904d22e9b7e
minseo <kim-west00@naver.com>
2021-02-06 23:09:39 +0900

    hw2-1예금잔고loop

--- OneDrive - 한국외국어대학교/바탕 화면/백준예제/forloop.py
+++ OneDrive - 한국외국어대학교/바탕 화면/백준예제/forloop.py
@@ -2,29 +2,8 @@ initial_balance= float(input("Initial balance?\n"))#원금입력
 Annual_interest_rate= float(input("Annual interest rate in percent?\n"))#년이자율 입력
 print('In case initial balance is {:.2f} \nand annual interest rate is {:.2f} percent,'.format(initial_balance,Annual_interest_rate)) #입력받은 원금과 년이자율을 출력
 balance = initial_balance#맨밑에 프린트되는 에서 차별성을 주기위해서 잔액을 balance 라고 이름을 설정했습니다
-balance= float(balance* ((Annual_interest_rate/100) +1))#1년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#2년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#3년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#4년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#5년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#6년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#7년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#8년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#9년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#10년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#11년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#12년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#13년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#14년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#15년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#16년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#17년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#18년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#19년후 잔액
-balance= float(balance* ((Annual_interest_rate/100) +1))#20년후 잔액
-
-
-
+for i in range(20): #20회 for 루프를 돌게함
+         initial=(1+rate/100.0) * initial
 
 
 print('final balance after 20 years will be {:.2f}'.format(balance)) #20년후 잔고 출력
